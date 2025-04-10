753fab83b274e485eeb18b87a94d75e56ccf204b
Sylwia <sylwiaklimkiewicz13@gmail.com>
2025-04-10 20:21:30 +0200

    main update

--- main.cpp
+++ main.cpp
@@ -1,45 +1,44 @@
 #include <iostream>
 #include <raylib.h>
 #include "grid.hpp"
+#include "simulation.hpp"
 
 int main() {
-    Color GREY = {255,255,255,255};
+    Color GREY = { 255,255,255,255 };
     const int WINDOW_WIDTH = 750;
     const int WINDOW_HEIGHT = 750;
     const int CELL_SIZE = 25;
-    
+
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
     SetTargetFPS(12);
 
-    Grid grid{WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE};
-    //losowe wypełnienie siatki
-    grid.FillRandom();
+    Simulation simulation{ WINDOW_WIDTH,WINDOW_HEIGHT,CELL_SIZE };
 
-    while(!WindowShouldClose()) {
-               // Obsługa zdarzeń
-               if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                Vector2 mousePosition = GetMousePosition();
-                int row = mousePosition.y / CELL_SIZE;
-                int col = mousePosition.x / CELL_SIZE;
-                grid.ToggleCell(row, col);
-    
-    }
-    
-    if(IsKeyPressed(KEY_C)) {
-        grid.Clear();
-    }
-    
-    if(IsKeyPressed(KEY_R)) {
-        grid.FillRandom();
-    }
-    
-    BeginDrawing();
-    ClearBackground(GREY);
-    grid.Draw();
-    EndDrawing();
-}
+    while (!WindowShouldClose()) {
+        // Obsługa zdarzeń
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+            Vector2 mousePosition = GetMousePosition();
+            int row = mousePosition.y / CELL_SIZE;
+            int col = mousePosition.x / CELL_SIZE;
+            simulation.ToggleCell(row, col);
 
-CloseWindow();
-return 0;
+        }
+
+        if (IsKeyPressed(KEY_C)) {
+            simulation.ClearGrid();
+        }
+
+        if (IsKeyPressed(KEY_R)) {
+            simulation.CreateRandomState();
+        }
+
+        BeginDrawing();
+        ClearBackground(GREY);
+        simulation.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 
 }
